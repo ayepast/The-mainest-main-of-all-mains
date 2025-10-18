4216e3796a0a3a6edb0e2ba0dffe7d8fad3296dd
ayepast <ayepast5@gmail.com>
2025-10-18 13:00:58 +0300

    Update Generator.py

--- Generator.py
+++ Generator.py
@@ -3,10 +3,8 @@ import random
 simb = "QWERTYUIOPASDFGHKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890+-!@#$%^&*"
 length = int(input("длина: "))
 password = ""
-cur = 0
-
-while cur < length:
-    cur += 1
+for i in range(0,length):
+   
     password = password + random.choice(simb)
 
 print(password)
